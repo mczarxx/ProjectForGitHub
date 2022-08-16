c883efd158a559ce3095578c52e33c0c062c6635
Mariusz Czarnecki <mczarxx@gmail.com>
2022-08-16 16:39:54 +0200

    added default constructor and equals and hashcode in models

--- src/main/java/pl/kurs/model/Employee.java
+++ src/main/java/pl/kurs/model/Employee.java
@@ -1,14 +1,32 @@
 package pl.kurs.model;
 
+import java.util.Objects;
+
 public class Employee {
     private String name;
     private String lastName;
     private double salary;
 
+    public Employee() {
+    }
+
     public Employee(String name, String lastName, double salary) {
         this.name = name;
         this.lastName = lastName;
         this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 && Objects.equals(name, employee.name) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName, salary);
     }
 
     @Override
